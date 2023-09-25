c07d30aa3c004d188ea5b6c4abc0dfb0ddb6ebd9
sabrina-goncalves-de-almeida <sabrina.goncalves@dcx.ufpb.br>
2023-09-25 00:14:31 -0300

    feat(#1): aula 5 - Funções

---
+++ aula 5 - funções/js/index.js
@@ -0,0 +1,11 @@
+function funcao() {
+    console.log("Sou uma função");
+}
+
+funcao();
+
+function mensagem(parametro1, parametro2, parametro3) {
+    console.log(parametro1, parametro2, parametro3);
+}
+
+mensagem("Oi", "meu", "jovem!");
